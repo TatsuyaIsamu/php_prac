41a44fdd53868cad69aff9d848a3f0fcc1ae7791
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-11-22 22:44:36 +0900

    feat(無し): 🎸 非同期通信機能作成"

--- todo_app_php_v4-master 2/work/app/Todo.php
+++ todo_app_php_v4-master 2/work/app/Todo.php
@@ -10,6 +10,7 @@ class Todo
     $this->pdo = $pdo;
   }
 
+  
   public function processPost()
   {
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -18,10 +19,14 @@ class Todo
 
     switch ($action) {
       case 'add':
-        $this->add();
+        $id = $this->add();
+        header('Content-Type: application/json');
+        echo json_encode(['id' => $id]);
         break;
       case 'toggle':
-        $this->toggle();
+        $isDone = $this->toggle();
+        header('Content-Type: application/json');
+        echo json_encode(['is_done' => $isDone]);
         break;
       case 'delete':
         $this->delete();
@@ -32,23 +37,25 @@ class Todo
       default:
         exit;
     }
-    header('Location: ' . SITE_URL);
+
     exit;
   }
   }
 
 
-private function add()
-{
-  $title = trim(filter_input(INPUT_POST, 'title'));
-  if ($title === '') {
-    return;
-  }
 
-  $stmt = $this->pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-  $stmt->bindValue('title', $title, \PDO::PARAM_STR);
-  $stmt->execute();
-}
+  private function add()
+  {
+    $title = trim(filter_input(INPUT_POST, 'title'));
+    if ($title === '') {
+      return;
+    }
+  
+    $stmt = $this->pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+    $stmt->bindValue('title', $title, \PDO::PARAM_STR);
+    $stmt->execute();
+    return (int) $this->pdo->lastInsertId();
+  }
 
 private function toggle()
 {
@@ -56,9 +63,19 @@ private function toggle()
   if (empty($id)) {
     return;
   }
+  $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE id = :id");
+  $stmt->bindValue('id', $id, \PDO::PARAM_INT);
+  $stmt->execute();
+  $todo = $stmt->fetch();
+  if (empty($todo)) {
+    header('HTTP', true, 404);
+    exit;
+  }
   $stmt = $this->pdo->prepare("UPDATE todos SET is_done = NOT is_done WHERE id = :id");
   $stmt->bindValue('id', $id, \PDO::PARAM_INT);
   $stmt->execute();
+
+  return (boolean) !$todo->is_done;
 }
 
 private function delete()
